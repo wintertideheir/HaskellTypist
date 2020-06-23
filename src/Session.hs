7bfe09ce1c7f32b99eea28425a8f5627e6b2f456
Sagun Singh <sagun.singh.vt@gmail.com>
2020-06-23 12:55:07 -0400

    Add Session module
    Add a Session module for the Session datatype to record a small segment
    of typing. Add auxiliary functions to record and render the session.

---
+++ src/Session.hs
@@ -0,0 +1,29 @@
+module Session where
+
+import qualified System.CPUTime (getCPUTime)
+
+data Session = Session { keystrokes :: [(Integer, Char)]
+                       , text       :: String
+                       }
+
+sessionFromText :: String -> Session
+sessionFromText t = Session { keystrokes = []
+                            , text       = t  }
+
+completeSession :: Session -> Bool
+completeSession s = (length $ keystrokes s) >= (length $ text s)
+
+recordKeystroke :: Session -> Char -> IO Session
+recordKeystroke s c =
+    if completeSession s
+    then return s
+    else do
+        t <- System.CPUTime.getCPUTime
+        return s { keystrokes = (keystrokes s) ++ [(t, c)] }
+
+renderKeytrokes :: Session -> (Char -> Maybe Bool -> a) -> [a]
+renderKeytrokes s t =
+    let matched    = zip (map snd (keystrokes s)) (text s)
+        complete   = map (\x -> t (snd x) (Just ((fst x) == (snd x)))) matched
+        incomplete = map (\x -> t x Nothing)                           (drop (length $ keystrokes s) $ text s)
+    in complete ++ complete
