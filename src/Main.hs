032d5c3ed8716db8fcd414add2d671d341181c77
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-10 19:03:54 -0400

    Rewrite drawing function to handle explicit newlines
    Rewrite `renderKeystrokes` to return a list of vertically stacked lines
    of rendered text. The rendering function is now expected to handle
    explicit newlines. This change preserves keystroke matching
    information.
    
    Add three new attributes to `themes` for drawing newlines. The new
    attributes are similar to default attributes, but the foreground and
    background are swapped.
    
    Rewrite the draw function to accept newlines in accordance with the
    changes above.

--- src/Main.hs
+++ src/Main.hs
@@ -20,15 +20,20 @@ initialSession = sessionFromText "Gallia est omnis divisa in partes tres, quarum
                                  \differunt. Gallos ab Aquitanis Garumna flumen, a \
                                  \Belgis Matrona et Sequana dividit."
 
-themes     :: AttrMap
-themeMiss  :: AttrName
-themeMatch :: AttrName
+themes       :: AttrMap
+themeMiss    :: AttrName
+themeMatch   :: AttrName
+themeNewline :: AttrName
 themes = attrMap (V.black `on` V.white)
-    [ (themeMiss,  bg $ V.rgbColor 255 150 150)
-    , (themeMatch, fg $ V.rgbColor 50 50 50)
+    [ (themeMiss,                  bg $ V.rgbColor 255 150 150)
+    , (themeMatch,                 fg $ V.rgbColor  50  50  50)
+    , (themeNewline,               V.white `on` V.black)
+    , (themeNewline <> themeMiss,  fg $ V.rgbColor 255  50  50)
+    , (themeNewline <> themeMatch, bg $ V.rgbColor 150 150 150)
     ]
-themeMiss  = attrName "miss"
-themeMatch = attrName "match"
+themeMiss    = attrName "miss"
+themeMatch   = attrName "match"
+themeNewline = attrName "newline"
 
 app :: App Session () ()
 app = App { appDraw         = drawFunction
@@ -39,20 +44,28 @@ app = App { appDraw         = drawFunction
 
 keyHandler :: Session -> BrickEvent () () -> EventM () (Next Session)
 keyHandler s (VtyEvent (V.EvKey V.KEsc []))      = halt s
-keyHandler s (VtyEvent (V.EvKey (V.KChar c) [])) = liftIO (recordKeystroke s c) >>= continue
+keyHandler s (VtyEvent (V.EvKey (V.KChar c) [])) = liftIO (recordKeystroke s c)    >>= continue
+keyHandler s (VtyEvent (V.EvKey V.KEnter    [])) = liftIO (recordKeystroke s '\n') >>= continue
 keyHandler s _                                   = continue s
 
 drawFunction :: Session -> [Widget ()]
 drawFunction s =
-    let applyTheme s (Just True)  = withAttr themeMatch $ str s
-        applyTheme s (Just False) = withAttr themeMiss  $ str s
-        applyTheme s Nothing      = showCursor () (Location (0, 0)) (str s)
+    let applyTheme s m =
+            if last s == '\n'
+            then applyThemeSafe (init s) m <+> themedNewline m
+            else applyThemeSafe s        m
+        applyThemeSafe s (Just True)  = withAttr themeMatch                   $ str s
+        applyThemeSafe s (Just False) = withAttr themeMiss                    $ str s
+        applyThemeSafe s Nothing      = showCursor () (Location (0, 0))       $ str s
+        themedNewline    (Just True)  = withAttr (themeNewline <> themeMatch) $ str "\\n"
+        themedNewline    (Just False) = withAttr (themeNewline <> themeMiss)  $ str "\\n"
+        themedNewline    Nothing      = showCursor () (Location (0, 0))
+                                      $ withAttr themeNewline                 $ str "\\n"
     in [withBorderStyle unicode
         $ borderWithLabel (str "Haskell Typist")
         $ center
         $ vBox
-        $ map vBox
-        $ map (map hBox) (renderKeystrokes s applyTheme)]
+        $ map hBox (renderKeystrokes s applyTheme)]
 
 main :: IO Session
 main = defaultMain app initialSession
--- src/Session.hs
+++ src/Session.hs
@@ -2,7 +2,7 @@ module Session where
 
 import qualified System.CPUTime (getCPUTime)
 import qualified Data.List.Extra (groupOn)
-import qualified Data.List.Split (splitWhen, chunksOf)
+import qualified Data.List.Split (split, whenElt, keepDelimsR, chunksOf)
 
 data Session = Session { keystrokes :: [(Integer, Char)]
                        , text       :: String
@@ -23,16 +23,20 @@ recordKeystroke s c =
         t <- System.CPUTime.getCPUTime
         return s { keystrokes = (keystrokes s) ++ [(t, c)] }
 
-renderKeystrokes :: Session -> (String -> Maybe Bool -> a) -> [[[a]]]
+renderKeystrokes :: Session -> (String -> Maybe Bool -> a) -> [[a]]
 renderKeystrokes s t =
     let remainingLength = (length $ text s) - (length $ keystrokes s)
         matchTuple '\n' (Just g) = ('\n', Just ('\n' == g))
         matchTuple e    (Just g) = (g,    Just (e    == g))
         matchTuple e    Nothing  = (e,    Nothing)
-    in map (map (map (\g -> t (map fst g) (snd $ head g))))
-       $ map (map (Data.List.Extra.groupOn snd))
+    in map (map (\g -> t (map fst g) (snd $ head g)))
+       $ map (Data.List.Extra.groupOn snd)
+       $ concat
        $ map (Data.List.Split.chunksOf 50)
-       $ Data.List.Split.splitWhen ((== '\n') . fst)
+       $ (Data.List.Split.split .
+          Data.List.Split.keepDelimsR .
+          Data.List.Split.whenElt)
+         ((== '\n') . fst)
        $ zipWith matchTuple (text s)
        $ (map Just $ map snd $ keystrokes s) ++
          (replicate remainingLength Nothing)
