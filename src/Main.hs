bf98929e3f4e543fa1e03a48bcf5c0d738cdeed0
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-13 20:33:41 -0400

    Extend sample session text

--- src/Main.hs
+++ src/Main.hs
@@ -18,7 +18,26 @@ initialSession = sessionFromText "Gallia est omnis divisa in partes tres, quarum
                                  \\n\
                                  \Hi omnes lingua, institutis, legibus inter se \
                                  \differunt. Gallos ab Aquitanis Garumna flumen, a \
-                                 \Belgis Matrona et Sequana dividit."
+                                 \Belgis Matrona et Sequana dividit.\
+                                 \\n\
+                                 \Horum omnium fortissimi sunt Belgae, propterea\ 
+                                 \quod a cultu atque humanitate provinciae \
+                                 \longissime absunt, minimeque ad eos mercatores \
+                                 \saepe commeant atque ea quae ad effeminandos \
+                                 \animos pertinent important,\
+                                 \\n\
+                                 \proximique sunt Germanis, qui trans Rhenum \
+                                 \incolunt, quibuscum continenter bellum gerunt. \
+                                 \Qua de causa Helvetii quoque reliquos Gallos \
+                                 \virtute praecedunt, quod fere cotidianis proeliis \
+                                 \cum Germanis contendunt, cum aut suis finibus eos \
+                                 \prohibent aut ipsi in eorum finibus bellum gerunt.\
+                                 \\n\
+                                 \Eorum una, pars, quam Gallos obtinere dictum est, \
+                                 \initium capit a flumine Rhodano, continetur \
+                                 \Garumna flumine, Oceano, finibus Belgarum, attingit \
+                                 \etiam ab Sequanis et Helvetiis flumen Rhenum, \
+                                 \vergit ad septentriones."
 
 themes       :: AttrMap
 themeMiss    :: AttrName
