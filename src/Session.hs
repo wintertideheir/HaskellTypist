d34bdb81e11f23a9a664855b90539d0d1b6afa4f
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-25 20:33:56 -0400

    Divide, extend, and rewrite the session type
    Rewrite the session type to reference a database system of passages and
    presets. Rather than keeping all of the program's data in the session
    datatype,
        * Add a passage fragment type to keep parts of each passage.
          Fragments will have text, and may contain audio. This change will
          allow text and audio to be freely interspersed.
        * Add a passage type to name and hold passage fragments. By
          grouping related fragments together, they make organization by
          the user easier.
        * Add a session preset type to hold references to session fragments
          similar to the passage type. This type will enable recombination
          of fragments from different sessions. This type also hold
          previous session data for tracking improvement. It isn't
          completely appropriate, but extraordinarily convenient.
        * Change the session type to merely record keystrokes and their
          time.
    
    Add HCodecs for the audio type and for future audio input processing.
    
    These changes **WILL** break the program.

--- src/Session.hs
+++ src/Session.hs
@@ -2,14 +2,36 @@ module Session where
 
 import qualified System.CPUTime (getCPUTime)
 import qualified Data.List.Extra (groupOn)
+import qualified Data.Audio (Audio)
 
-data Session = Session { keystrokes :: [(Integer, Char)]
-                       , text       :: String
-                       }
+-- |A fragment, often a sentence, of a passage.
+data PassageFragment = PassageTextFragment String
+                     | PassageAudioFragment String (Data.Audio.Audio Float)
 
-sessionFromString :: String -> Session
-sessionFromString t = Session { keystrokes = []
-                              , text       = t  }
+-- |A complete passage, with it's identifier, name,
+-- and list of fragments.
+data Passage = Passage { passageId        :: Int,
+                         passageName      :: String
+                         passageFragments :: [PassageFragment]}
+
+-- |A session preset, composed of it's identifier, name,
+-- a list of passage and fragment indicies, and previous
+-- session data. Passage identifiers with an empty list are
+-- interpreted as including the entire passage.
+data SessionPreset = SessionPreset { sessionId        :: Int,
+                                     sessionNam       :: String,
+                                     sessionFragments :: [(Int, [Int])],
+                                     sessionPrevious  :: [Session] }
+
+-- |A typing session, which is a list of typed characters and
+-- their time in picoseconds.
+data Session = Session [(Integer, Char)]
+
+data TypistData = TypistData [Passage] [SessionPreset]
+
+-------------------------------------------------------------
+-- TODO: Rewrite the below code to utilize the above types --
+-------------------------------------------------------------
 
 sessionComplete :: Session -> Bool
 sessionComplete s = (length $ keystrokes s) >= (length $ text s)
