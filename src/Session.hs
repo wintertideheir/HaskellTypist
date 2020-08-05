4213d21ef1f469aa266ae424b0b41febae0fc0df
Sagun Singh <sagun.singh.vt@gmail.com>
2020-08-05 12:57:49 -0400

    Change single line style to multiline style for Haddock documentation

--- src/Session.hs
+++ src/Session.hs
@@ -7,7 +7,9 @@ import qualified Data.Text         (pack)
 import qualified Data.List
 import qualified Data.Text.Metrics (damerauLevenshteinNorm)
 
--- |A fragment, often a sentence, of a passage.
+{-|
+    A fragment, often a sentence, of a passage.
+-}
 data PassageFragment = PassageTextFragment String
                      | PassageAudioFragment String (Data.Audio.Audio Float)
 
@@ -73,25 +75,29 @@ consumeFragments pfs s  =
             else consumeFragments' (pf':pfs') s' (l+1)
     in consumeFragments' pfs s 0
 
--- |A complete passage, with it's identifier, name,
--- and list of fragments.
+{-|
+    A complete passage, with it's identifier, name, and list of fragments.
+-}
 data Passage = Passage { passageId        :: Int
                        , passageName      :: String
                        , passageDate      :: Data.Time.Clock.UTCTime
                        , passageFragments :: [PassageFragment]
                        }
 
--- |Retrive a list of passage fragments given their indices. Out of
--- bounds indices are simply discarded.
+{-|
+    Retrive a list of passage fragments given their indices. Out of bounds
+    indices are simply discarded.
+-}
 subPassage :: Passage -> [Int] -> [PassageFragment]
 subPassage p i =
     map ((passageFragments p) !!)
     $ filter (>= 0)
     $ filter (< length (passageFragments p)) i
 
--- |A session preset, composed of it's identifier, name,
--- a list of passage and fragment indicies, and previous
--- session data.
+{-|
+    A session preset, composed of it's identifier, name, a list of passage
+    and fragment indicies, and previous session data.
+-}
 data SessionPreset = SessionPreset { sessionId        :: Int
                                    , sessionName      :: String
                                    , sessionDate      :: Data.Time.Clock.UTCTime
@@ -99,18 +105,20 @@ data SessionPreset = SessionPreset { sessionId        :: Int
                                    , sessionPrevious  :: [Session]
                                    }
 
--- |A typing session, which is a list of typed characters and
--- their time in picoseconds.
+{-|
+    A typing session, which is a list of typed characters and their time in
+    picoseconds.
+-}
 data Session = Session Data.Time.Clock.UTCTime [(Integer, Char)]
 
 data TypistData = TypistData [Passage] [SessionPreset]
 
--- |Constructor for a passage in a 'TypistData'
--- given its name and list of fragments. Will
--- assign the smallest unique, nonnegative
--- integer identifier, and fails through 'head'
--- otherwise. Records the date of creation through
--- 'Data.Time.Clock.getCurrentTime'.
+{-|
+    Constructor for a passage in a 'TypistData' given its name and list of
+    fragments. Will assign the smallest unique, nonnegative integer
+    identifier, and fails through 'head' otherwise. Records the date of
+    creation through 'Data.Time.Clock.getCurrentTime'.
+-}
 addPassage :: TypistData -> String -> [PassageFragment] -> IO TypistData
 addPassage (TypistData ps sps) name pfs =
     do t <- Data.Time.Clock.getCurrentTime
@@ -123,13 +131,12 @@ addPassage (TypistData ps sps) name pfs =
                        }
        return (TypistData (p:ps) sps)
 
--- |Constructor for a session preset in a
--- 'TypistData' given its name and list of session
--- and fragment indices. Will assign the smallest
--- unique, nonnegative integer identifier, and
--- fails through 'head' otherwise. Records the date
--- of creation through
--- 'Data.Time.Clock.getCurrentTime'.
+{-|
+    Constructor for a session preset in a 'TypistData' given its name and
+    list of session and fragment indices. Will assign the smallest unique,
+    nonnegative integer identifier, and fails through 'head' otherwise.
+    Records the date of creation through 'Data.Time.Clock.getCurrentTime'.
+-}
 addPreset :: TypistData -> String -> [(Int, [Int])] -> IO TypistData
 addPreset (TypistData ps sps) name sfs =
     do t <- Data.Time.Clock.getCurrentTime
@@ -143,12 +150,12 @@ addPreset (TypistData ps sps) name sfs =
                               }
        return (TypistData ps (sp:sps))
 
--- |Appends a session to its session preset in a
--- 'TypistData'. If the given session preset
--- identifer does not exist, fails through
--- incomplete pattern matching. Records the date of
--- addition through
--- 'Data.Time.Clock.getCurrentTime'.
+{-|
+    Appends a session to its session preset in a 'TypistData'. If the given
+    session preset identifer does not exist, fails through incomplete
+    pattern matching. Records the date of addition through
+    'Data.Time.Clock.getCurrentTime'.
+-}
 addSession :: TypistData -> Int -> [(Integer, Char)] -> IO TypistData
 addSession (TypistData ps sps) identifer ks =
     do t <- Data.Time.Clock.getCurrentTime
@@ -157,9 +164,11 @@ addSession (TypistData ps sps) identifer ks =
            sp' = sp { sessionPrevious = s : sessionPrevious sp }
        return (TypistData ps (sps1 ++ (sp':sps2)))
 
--- |Retrive a list of passage fragments from different passages given the
--- passage identifiers and fragments indices. Out of bounds indices and
--- identifers are simply discarded.
+{-|
+    Retrive a list of passage fragments from different passages given the
+    passage identifiers and fragments indices. Out of bounds indices and
+    identifers are simply discarded.
+-}
 subPassages :: TypistData -> [(Int, [Int])] -> [PassageFragment]
 subPassages (TypistData p _) identifiers =
     let subPassages' []           = []
@@ -169,7 +178,9 @@ subPassages (TypistData p _) identifiers =
                 Just j  -> (subPassage j x2) : (subPassages' xs)
     in concat $ subPassages' identifiers
 
--- |Consume a string based on a session preset identifier.
+{-|
+    Consume a string based on a session preset identifier.
+-}
 consumePreset :: TypistData -> Int -> String -> [(Char, Maybe Float)]
 consumePreset td@(TypistData _ sps) identifier s =
     case Data.List.find ((== identifier) . sessionId) sps of
