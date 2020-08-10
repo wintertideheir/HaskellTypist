7ce90b4a5473eb10f64839789b01f468025f813d
Sagun Singh <sagun.singh.vt@gmail.com>
2020-08-10 16:37:37 -0400

    Refactor `Session` module
    Rewrite types in the `Session` from records to simple contructors.
    
    Refactor a reference and keystroke type out from the preset type.
    
    Change the keystroke time type to `Data.Time.Clock.System.SystemChar`,
    which is far more convenient to work with.
    
    Refactor a metadata type out of the preset and passage types,
    simplifying constructors.
    
    Rename constructor functions to begin with `new`. Rename and combine
    `subPassage` and `subPassages` to `derefence`.
    
    Break code into sections with various 72 character wide banners.

--- src/Session.hs
+++ src/Session.hs
@@ -1,109 +1,112 @@
 module Session where
 
-import qualified Data.List.Extra   (groupOn)
-import qualified Data.Audio        (Audio)
-import qualified Data.Time.Clock   (UTCTime, getCurrentTime)
-import qualified Data.Text         (pack)
+import qualified Data.List.Extra         (groupOn)
+import qualified Data.Audio              (Audio)
+import qualified Data.Time.Clock         (UTCTime, getCurrentTime)
+import qualified Data.Time.Clock.System  (SystemTime)
+import qualified Data.Text               (pack)
 import qualified Data.List
-import qualified Data.Text.Metrics (damerauLevenshteinNorm)
+import qualified Data.Text.Metrics       (damerauLevenshteinNorm)
 
-data PassageFragment = PassageTextFragment String
-                     | PassageAudioFragment String (Data.Audio.Audio Float)
+----------------------------------------------------------------------
+--                              Types                               --
+----------------------------------------------------------------------
 
-data Passage = Passage { passageId        :: Int
-                       , passageName      :: String
-                       , passageDate      :: Data.Time.Clock.UTCTime
-                       , passageFragments :: [PassageFragment]
-                       }
+data Metadata = Metadata { uid  :: Int
+                         , name :: String
+                         , date :: Data.Time.Clock.UTCTime
+                         }
 
-data SessionPreset = SessionPreset { sessionId        :: Int
-                                   , sessionName      :: String
-                                   , sessionDate      :: Data.Time.Clock.UTCTime
-                                   , sessionFragments :: [(Int, [Int])]
-                                   , sessionPrevious  :: [Session]
-                                   }
+data Fragment = FragmentText  String
+              | FragmentAudio String (Data.Audio.Audio Float)
 
-data Session = Session Data.Time.Clock.UTCTime [(Integer, Char)]
+data Passage = Passage Metadata [Fragment]
 
-data TypistData = TypistData [Passage] [SessionPreset]
+data Reference = ReferenceAll  Int
+               | ReferencePart Int [Int]
+
+data Preset = Preset Metadata [Reference] [Session]
+
+data Keystroke = Keystroke Data.Time.Clock.System.SystemTime Char
+
+data Session = Session Data.Time.Clock.UTCTime [Keystroke]
+
+data TypistData = TypistData [Passage] [Preset]
+
+----------------------------------------------------------------------
+--                           Constuctors                            --
+----------------------------------------------------------------------
 
 {-|
-    Constructor for a passage in a 'TypistData'. Will assign the smallest
-    unique, nonnegative integer identifier, and fails through 'head'
-    otherwise. Records the date of creation through
-    'Data.Time.Clock.getCurrentTime'.
+    Create new 'Metadata' given a list of existing 'Metadata' and a name.
+    All non-negative values of 'Int' are possible identifiers. If a unique
+    identifier cannot be found, throw an error. The date recorded is the time
+    of evaluation.
 -}
-addPassage :: TypistData -> String -> [PassageFragment] -> IO TypistData
-addPassage (TypistData ps sps) name pfs =
+newMetadata :: [Metadata] -> String -> IO Metadata
+newMetadata m name' =
+    case [0..maxBound] Data.List.\\ (map uid m) of
+         []       -> error ("No unique identifier possible for \"" ++ show name' ++ "\".")  
+         (uid':_) -> do t <- Data.Time.Clock.getCurrentTime
+                        return Metadata { uid  = uid'
+                                        , name = name'
+                                        , date = t
+                                        }
+
+newPassage :: TypistData -> String -> [Fragment] -> IO TypistData
+newPassage (TypistData p1 p2) n f =
+    do m <- newMetadata (map (\(Passage m' _) -> m') p1) n
+       return (TypistData (Passage m f : p1) p2)
+
+newPreset :: TypistData -> String -> [Reference] -> IO TypistData
+newPreset (TypistData p1 p2) n r =
+    do m <- newMetadata (map (\(Preset m' _ _) -> m') p2) n
+       return (TypistData p1 (Preset m r [] : p2))
+
+newSession :: TypistData -> Int -> [Keystroke] -> IO TypistData
+newSession (TypistData p1 p2) i k =
     do t <- Data.Time.Clock.getCurrentTime
-       let p = Passage { passageId        = head ([0..(maxBound :: Int)]
-                                                  Data.List.\\
-                                                  (map passageId ps))
-                       , passageName      = name
-                       , passageDate      = t
-                       , passageFragments = pfs
-                       }
-       return (TypistData (p:ps) sps)
+       let modifyOrError _ _ e []     = e
+           modifyOrError c trans e (x:xs) =
+               if c x
+               then (trans x) : xs
+               else x         : (modifyOrError c trans e xs)
+           p2' = modifyOrError
+                     (\(Preset m _ _) -> uid m == i)
+                     (\(Preset m r s) -> Preset m r (Session t k : s))
+                     (error ("No preset with UID " ++ show i ++ " to add session to."))
+                     p2
+       return (TypistData p1 p2')
+
+----------------------------------------------------------------------
+--                            Retrieval                             --
+----------------------------------------------------------------------
 
 {-|
-    Constructor for a session preset in a 'TypistData'. Will assign the
-    smallest unique, nonnegative integer identifier, and fails through
-    'head' otherwise. Records the date of creation through
-    'Data.Time.Clock.getCurrentTime'.
+    Retrive a list of passage fragments given a reference. Out of bounds
+    references throw errors.
 -}
-addPreset :: TypistData -> String -> [(Int, [Int])] -> IO TypistData
-addPreset (TypistData ps sps) name sfs =
-    do t <- Data.Time.Clock.getCurrentTime
-       let sp = SessionPreset { sessionId        = head ([0..(maxBound :: Int)]
-                                                         Data.List.\\
-                                                         (map sessionId sps))
-                              , sessionName      = name
-                              , sessionDate      = t
-                              , sessionFragments = sfs
-                              , sessionPrevious  = []
-                              }
-       return (TypistData ps (sp:sps))
+dereference :: TypistData -> Reference -> [Fragment]
+dereference (TypistData p _) (ReferenceAll  uid'  ) =
+    case Data.List.find (\(Passage m _) -> uid m == uid') p of
+        Just (Passage _ f) -> f
+        Nothing            -> error ("No passage with UID " ++ show uid' ++ ".")
+dereference (TypistData p _) (ReferencePart uid' i) =
+    let error1 = error ("Out of bounds passage fragment with passage UID " ++ show uid' ++ ".")
+        error2 = error ("No passage with UID "                             ++ show uid' ++ ".")
+        indexFragment f i' = if i' > (length f)
+                            then error1
+                            else f !! i'
+    in case Data.List.find (\(Passage m _) -> uid m == uid') p of
+           Just (Passage _ f) -> map (indexFragment f) i
+           Nothing            -> error2
+
+----------------------------------------------------------------------
+--                              Render                              --
+----------------------------------------------------------------------
 
 {-|
-    Appends a session to its session preset in a 'TypistData'. If the given
-    session preset identifer does not exist, fails through incomplete
-    pattern matching. Records the date of addition through
-    'Data.Time.Clock.getCurrentTime'.
--}
-addSession :: TypistData -> Int -> [(Integer, Char)] -> IO TypistData
-addSession (TypistData ps sps) identifer ks =
-    do t <- Data.Time.Clock.getCurrentTime
-       let (sps1, (sp : sps2)) = break ((== identifer) . sessionId) sps
-           s = Session t ks
-           sp' = sp { sessionPrevious = s : sessionPrevious sp }
-       return (TypistData ps (sps1 ++ (sp':sps2)))
-
-{-|
-    Retrive a list of passage fragments given their indices. Out of bounds
-    indices are simply discarded.
--}
-subPassage :: Passage -> [Int] -> [PassageFragment]
-subPassage p i =
-    map ((passageFragments p) !!)
-    $ filter (>= 0)
-    $ filter (< length (passageFragments p)) i
-
-{-|
-    Retrive a list of passage fragments from different passages given the
-    passage identifiers and fragments indices. Out of bounds indices and
-    identifers are simply discarded.
--}
-subPassages :: TypistData -> [(Int, [Int])] -> [PassageFragment]
-subPassages (TypistData p _) identifiers =
-    let subPassages' []           = []
-        subPassages' ((x1,x2):xs) =
-            case Data.List.find ((== x1) . passageId) p of
-                Nothing ->                     (subPassages' xs)
-                Just j  -> (subPassage j x2) : (subPassages' xs)
-    in concat $ subPassages' identifiers
-
-{-|
-    Consume a fragment from typed input. Taking a 'PassageFragment' and
+    Consume a fragment from typed input. Taking a 'Fragment' and
     a 'String', render a list of characters with a possible normalized
     score.
 
@@ -111,24 +114,24 @@ subPassages (TypistData p _) identifiers =
     > consumeFragment s == consumeFragment (s ++ s')
     for any string s'. The string s \"completes\" the fragment.
 
-    If the fragment is a 'PassageTextFragment', compare each typed
+    If the fragment is a 'FragmentText', compare each typed
     character to the fragment and score it, rendering the entire fragment.
 
-    If the fragment is a 'PassageAudioFragment', first decide whether
+    If the fragment is a 'FragmentAudio', first decide whether
     the fragment is complete when the typed input is at least as long as
     the fragment itself, by checking if the string metric improves with
-    consuming more characters. If a 'PassageAudioFragment' is complete,
+    consuming more characters. If a 'FragmentAudio' is complete,
     then render the fragment with it's string metrics, otherwise return
     the typed input.
 -}
-consumeFragment :: PassageFragment -> String -> [(Char, Maybe Float)]
-consumeFragment (PassageTextFragment [])     _      = []
-consumeFragment (PassageTextFragment f)      []     = [(f', Nothing) | f' <- f]
-consumeFragment (PassageTextFragment (f:fs)) (s:ss) =
+consumeFragment :: Fragment -> String -> [(Char, Maybe Float)]
+consumeFragment (FragmentText [])     _      = []
+consumeFragment (FragmentText f)      []     = [(f', Nothing) | f' <- f]
+consumeFragment (FragmentText (f:fs)) (s:ss) =
     if f == s
-    then (f, Just 1.0) : consumeFragment (PassageTextFragment fs) ss
-    else (f, Just 0.0) : consumeFragment (PassageTextFragment fs) ss
-consumeFragment (PassageAudioFragment f _) s =
+    then (f, Just 1.0) : consumeFragment (FragmentText fs) ss
+    else (f, Just 0.0) : consumeFragment (FragmentText fs) ss
+consumeFragment (FragmentAudio f _) s =
     let subMetric l = fromRational
                       $ toRational
                       $ Data.Text.Metrics.damerauLevenshteinNorm
@@ -153,23 +156,13 @@ consumeFragment (PassageAudioFragment f _) s =
     The input string is treated as a substring of the concatenation of
     substrings that complete each fragment in the given list. 
 -}
-consumeFragments :: [PassageFragment] -> String -> [(Char, Maybe Float)]
+consumeFragments :: [Fragment] -> String -> [(Char, Maybe Float)]
 consumeFragments pfs "" = concat [consumeFragment pf "" | pf <- pfs]
 consumeFragments pfs s  =
     let consumeFragments' pfs' "" _ = consumeFragments pfs' ""
-        consumeFragments' []   [] _ = []
+        consumeFragments' []   x  _ = zip x (repeat Nothing)
         consumeFragments' (pf':pfs') s' l =
             if consumeFragment pf' (take l s') == consumeFragment pf' (take (l+1) s')
             then (consumeFragment pf' (take l s')) ++ (consumeFragments' pfs' (drop l s') 0)
             else consumeFragments' (pf':pfs') s' (l+1)
     in consumeFragments' pfs s 0
-
-{-|
-    Consume a string based on a session preset identifier.
--}
-consumePreset :: TypistData -> Int -> String -> [(Char, Maybe Float)]
-consumePreset td@(TypistData _ sps) identifier s =
-    case Data.List.find ((== identifier) . sessionId) sps of
-        Just sp -> consumeFragments (subPassages td
-                                     $ sessionFragments sp) s
-        Nothing -> []
