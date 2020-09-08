4bb9163de0077e3e207848e8c749ed17c7941fe2
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-08 07:22:06 -0400

    Move rgbColorInt to theme declarations
    Move rgbColorInt to theme declarations to minimize namespace pollution
    and enforce locality of local functions.

--- src/Themes.hs
+++ src/Themes.hs
@@ -8,18 +8,18 @@ themeMiss    :: AttrName
 themeMatch   :: AttrName
 themeNormal  :: AttrName
 themeSpecial :: AttrName
-themes = attrMap (black `on` white)
-    [ (themeNormal,                black `on` white)
-    , (themeNormal <> themeMiss,   bg $ rgbColorInt 255 150 150)
-    , (themeNormal <> themeMatch,  fg $ rgbColorInt  50  50  50)
-    , (themeSpecial,               bg $ rgbColorInt 200 200 200)
-    , (themeSpecial <> themeMiss,  fg $ rgbColorInt 255  50  50)
-    , (themeSpecial <> themeMatch, fg $ rgbColorInt 150 150 150)
-    ]
+themes =
+    let rgbColorInt :: Int -> Int -> Int -> Color
+        rgbColorInt = rgbColor
+    in attrMap (black `on` white)
+           [ (themeNormal,                black `on` white)
+           , (themeNormal <> themeMiss,   bg $ rgbColorInt 255 150 150)
+           , (themeNormal <> themeMatch,  fg $ rgbColorInt  50  50  50)
+           , (themeSpecial,               bg $ rgbColorInt 200 200 200)
+           , (themeSpecial <> themeMiss,  fg $ rgbColorInt 255  50  50)
+           , (themeSpecial <> themeMatch, fg $ rgbColorInt 150 150 150)
+           ]
 themeMiss    = attrName "miss"
 themeMatch   = attrName "match"
 themeNormal  = attrName "normal"
 themeSpecial = attrName "special"
-
-rgbColorInt :: Int -> Int -> Int -> Color
-rgbColorInt = rgbColor
