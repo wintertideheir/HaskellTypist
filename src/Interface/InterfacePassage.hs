ebc454cec2f241c76252c4a1f3daefb57cd9a2ad
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-09 15:05:53 -0400

    Implement list drawing interface for passage selection

--- src/Interface/InterfacePassage.hs
+++ src/Interface/InterfacePassage.hs
@@ -4,9 +4,16 @@ import Passage
 import Interface
 
 import qualified Brick as B
+import qualified Brick.Widgets.List as BL
 
-data InterfacePassage = InterfacePassage { passages   :: [Passage] }
+import qualified Data.Functor
+
+instance BL.Splittable [] where
+    splitAt n x = (take n x, drop n x)
+    slice i n = take n . drop i
+
+data InterfacePassage = InterfacePassage { passage :: BL.GenericList () [] Passage }
 
 instance Interface InterfacePassage where
     input interface _ = B.continue interface
-    draw _ = B.str "Nothing yet!"
+    draw interface = BL.renderList (\_ e -> B.str e.name) True (passage interface)
