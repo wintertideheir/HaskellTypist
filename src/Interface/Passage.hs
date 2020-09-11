1621ff0d944bb7c158622d8d7a9bfb3b2dd22e16
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-11 05:02:27 -0400

    Add simple passage list interface
    Implement a simple list showing the first 5 passages of a IPassage
    interface. This interface will be used to browse passages.

--- src/Interface/Passage.hs
+++ src/Interface/Passage.hs
@@ -11,4 +11,14 @@ data IPassage = IPassage { passages :: [Passage] }
 instance Interface IPassage where
     input interface (B.VtyEvent (V.EvKey V.KEsc [])) = B.halt interface
     input interface _ = B.continue interface
-    draw _ = B.str "Nothing yet!"
+    draw interface =
+        if null interface.passages
+        then B.str "Nothing yet!"
+        else let column n f = B.padLeft (B.Pad 1)
+                            $ B.vBox
+                            $ ((B.str n):)
+                            $ map (B.str . f)
+                            $ take 5 interface.passages
+             in B.hBox [column "ID"      (show . uid),
+                        column "Passage" name,
+                        column "Date"    (show . date)]
