fd297cba3ea703bc958e40019edc36efb37487fd
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-11 04:25:34 -0400

    Add exit key to IPassage
    Copy exit key from ISession to IPassage to allow a developer to easily
    exit the program.

--- src/Interface/Passage.hs
+++ src/Interface/Passage.hs
@@ -4,9 +4,11 @@ import Passage
 import Interface
 
 import qualified Brick as B
+import qualified Graphics.Vty as V
 
 data IPassage = IPassage { passage :: [Passage] }
 
 instance Interface IPassage where
+    input interface (B.VtyEvent (V.EvKey V.KEsc [])) = B.halt interface
     input interface _ = B.continue interface
     draw _ = B.str "Nothing yet!"
