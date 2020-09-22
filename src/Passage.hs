01b1c8ec21f08f72c14c72b71e2f30c70bb6b2d5
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-22 06:38:47 -0400

    Replace HasDefault class with undefined value
    Remove the HasDefault class, which provided defaultValue as the
    serialized value of the Transient type. Replace it with the undefined
    value. This change takes advantage of Haskell's lazy evaluation to
    simplify code. Documentation on the use of Transient should be added in
    the future, either to haskell-typist or to flat.

--- src/Passage.hs
+++ src/Passage.hs
@@ -37,15 +37,9 @@ data Transient a = Transient a
 instance Functor Transient where
     fmap f (Transient a) = Transient (f a)
 
-class HasDefault a where
-    defaultValue :: a
-
-instance HasDefault [a] where
-    defaultValue = []
-
-instance (HasDefault a) => Flat.Flat (Transient a) where
+instance Flat.Flat (Transient a) where
     encode _ = Flat.encode ()
-    decode   = (Transient defaultValue) <$ (Flat.decode :: Flat.Decoder.Types.Get ())
+    decode   = (Transient undefined) <$ (Flat.decode :: Flat.Decoder.Types.Get ())
     size   _ = Flat.size ()
 
 -----------------------------------------------------------------------
