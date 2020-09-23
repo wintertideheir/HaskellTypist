b76a7e0cb13b0157c16e29d5b10704973f5ae9f8
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-23 08:32:47 -0400

    Rewrite the Focus module
    Rewrite the Focus module, and the FocusedList type, to work with
    non-empty lists. This change simplifies our code and still works within
    the requirements of our application.

--- src/Data/Focus.hs
+++ src/Data/Focus.hs
@@ -1,42 +1,25 @@
 module Data.Focus where
 
-modifyNth :: [a] -> Int -> (a -> a) -> [a]
-modifyNth l i f =  take i l
-                ++ [f (l !! i)]
-                ++ drop (i + 1) l
+data FocusedList a = FocusedList { before :: [a]
+                                 , focus :: a
+                                 , after  :: [a]
+                                 }
 
-data FocusedList a = FocusedList   [a] Int
-                   | UnfocusedList [a]
+instance Foldable FocusedList where
+   foldMap l = foldMap (l.before ++ [l.focus] ++ l.after)
 
-mapFocus :: (a -> a) -> FocusedList a -> FocusedList a
-mapFocus f (FocusedList l i) = FocusedList (modifyNth l i f) i
-mapFocus _ (UnfocusedList l) = UnfocusedList l
+focusUp :: FocusedList a -> FocusedList a
+focusDown l = if null l.before
+              then l
+              else FocusedList { before = init l.before
+                               , focus  = last l.before
+                               , after  = (l.focus):(l.after)
+                               }
 
-focusI :: FocusedList a -> Maybe Int
-focusI (FocusedList _ i) = Just i
-focusI (UnfocusedList _) = Nothing
-
-focusA :: FocusedList a -> Maybe a
-focusA (FocusedList l i) = Just (l !! i)
-focusA (UnfocusedList l) = Nothing
-
-refocus ::  Int -> FocusedList a -> FocusedList a
-refocus i l = let l' = unwrap l
-              in if (length l' > i)
-                 then FocusedList l' i
-                 else l
-
-unfocus :: FocusedList a -> FocusedList a
-unfocus (FocusedList l _) = UnfocusedList l
-unfocus (UnfocusedList l) = UnfocusedList l
-
-unwrap :: FocusedList a -> [a]
-unwrap (FocusedList l _) = l
-unwrap (UnfocusedList l) = l
-
-wrap :: [a] -> FocusedList a
-wrap = UnfocusedList
-
-rewrap :: ([a] -> [a]) -> FocusedList a -> FocusedList a
-rewrap f (FocusedList l i) = refocus i $ FocusedList   (f l) i
-rewrap f (UnfocusedList l) = refocus i $ UnfocusedList (f l)
+focusDown :: FocusedList a -> FocusedList a
+focusDown l = if null l.after
+              then l
+              else FocusedList { before = l.before ++ [l.focus]
+                               , focus  = head l.after
+                               , after  = tail l.after
+                               }
