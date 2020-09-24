48816f8ee0599f7529bbc431ead716d0be6f368d
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-23 21:42:18 -0400

    Fix focusUp function implementation name

--- src/Data/Focus.hs
+++ src/Data/Focus.hs
@@ -1,15 +1,18 @@
 module Data.Focus where
 
 data FocusedList a = FocusedList { before :: [a]
-                                 , focus :: a
+                                 , focus  :: a
                                  , after  :: [a]
                                  }
 
-instance Foldable FocusedList where
-   foldMap l = foldMap (l.before ++ [l.focus] ++ l.after)
+focusList :: [a] -> FocusedList a
+focusList f = FocusedList { before = []
+                          , focus  = head f
+                          , after  = tail f
+                          }
 
 focusUp :: FocusedList a -> FocusedList a
-focusDown l = if null l.before
+focusUp l = if null l.before
               then l
               else FocusedList { before = init l.before
                                , focus  = last l.before
